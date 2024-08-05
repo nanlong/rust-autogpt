0997592517cf747811de863b61e131c5de5c17f0
Jeff <fei.code@gmail.com>
2024-08-05 11:04:26 +0800

    feat: print agent activity to command line

--- auto-gippity/src/helpers/command_line.rs
+++ auto-gippity/src/helpers/command_line.rs
@@ -4,6 +4,43 @@ use crossterm::{
 };
 use std::io;
 
+#[derive(PartialEq, Debug)]
+pub enum PrintCommand {
+    AICall,
+    UnitTest,
+    Issue,
+}
+
+impl PrintCommand {
+    pub fn print_agent_message(
+        &self,
+        agent_pos: &str,
+        agent_statement: &str,
+    ) -> anyhow::Result<()> {
+        let mut stdout = io::stdout();
+
+        // Decide on the print color
+        let statement_color = match self {
+            PrintCommand::AICall => Color::Cyan,
+            PrintCommand::UnitTest => Color::Magenta,
+            PrintCommand::Issue => Color::Red,
+        };
+
+        // Print the agent statement
+        stdout.execute(SetForegroundColor(Color::Green))?;
+        print!("Agent: {}: ", agent_pos);
+
+        // Make select color
+        stdout.execute(SetForegroundColor(statement_color))?;
+        println!("{}", agent_statement);
+
+        // Reset the color
+        stdout.execute(ResetColor)?;
+
+        Ok(())
+    }
+}
+
 // Get user request
 pub fn get_user_response(question: &str) -> anyhow::Result<String> {
     let mut stdout = io::stdout();
@@ -23,4 +60,18 @@ pub fn get_user_response(question: &str) -> anyhow::Result<String> {
         .expect("Failed to read response");
 
     Ok(user_response.trim().to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_prints_agent_msg() {
+        let print_command = PrintCommand::AICall;
+        let agent_pos = "Managing Agent";
+        let agent_statement = "Testing testing, processing something";
+        let result = print_command.print_agent_message(agent_pos, agent_statement);
+        assert_eq!(result.is_ok(), true);
+    }
 }
